45a1df3667ea3d99092a22af5f0334f204c58405
Raj Judge <43121310+judgeraj@users.noreply.github.com>
2022-04-28 22:02:40 -0700

    Minor Changes

---
+++ calendar/Cell.swift
@@ -0,0 +1,12 @@
+//
+//  Cell.swift
+//  calendar
+//
+//  Created by Rajpreet Judge on 4/28/22.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+}
--- calendar/ViewController.swift
+++ calendar/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelOfMonth: UILabel!
+    
+    let monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+    var count = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelOfMonth.text = monthArray[count]
         // Do any additional setup after loading the view.
     }
 
-
+    
+    func setCells() {
+        let width = (collectionView.frame.size.width)/8
+        let height = (collectionView.frame.size.height)/8
+        
+        
+    }
+    
+    
+    @IBAction func nextMonth(_ sender: Any) {
+        print("before if" , String(count))
+        if count >= 11 {
+            count = 0
+            labelOfMonth.text = monthArray[count]
+        } else {
+            count += 1
+            labelOfMonth.text = monthArray[count]
+        }
+        
+        print("after if", String(count))
+    }
+    
+    @IBAction func prevMonth(_ sender: Any) {
+        print("before if", String(count))
+        if count <= 0 {
+            count = 11
+            labelOfMonth.text = monthArray[count]
+        } else {
+            count -= 1
+            labelOfMonth.text = monthArray[count]
+        }
+        print("after if", String(count))
+//        labelOfMonth.text = monthArray[count]
+    }
 }
 
